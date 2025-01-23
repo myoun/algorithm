fac764230e1c60572457a4648bdabb3d27e4b5ab
Myoun <aiden080605@gmail.com>
2025-01-23 16:32:23 +0900

    refactor: Rename apply_priority functions and improve postfix conversion

--- 1918.py
+++ 1918.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass
-from typing import Self
 
 expression = input()
 
@@ -36,7 +35,7 @@ for i, c in enumerate(expression):
         else:
             parsed.append(Term(c))
 
-def apply_priority(ast):
+def apply_priority1(ast):
     new_ast=[]
     p = 0
 
@@ -51,11 +50,36 @@ def apply_priority(ast):
             del new_ast[-1]
             next = ast[i+1]
             if isinstance(ast[i+1], list):
-                next = apply_priority(ast[i+1])
+                next = apply_priority1(ast[i+1])
             new_ast.append([prev, e, next])
             p = 1
         elif isinstance(e, list):
-            new_ast.append(apply_priority(e))
+            new_ast.append(apply_priority1(e))
+        else:
+            new_ast.append(e)
+    
+    return new_ast
+
+def apply_priority2(ast):
+    new_ast=[]
+    p = 0
+
+    for i, e in enumerate(ast):
+
+        if p > 0:
+            p -= 1
+            continue
+
+        if isinstance(e, Operator) and (e.value == "+" or e.value =="-"):
+            prev = new_ast[-1]
+            del new_ast[-1]
+            next = ast[i+1]
+            if isinstance(ast[i+1], list):
+                next = apply_priority2(ast[i+1])
+            new_ast.append([prev, e, next])
+            p = 1
+        elif isinstance(e, list):
+            new_ast.append(apply_priority2(e))
         else:
             new_ast.append(e)
     
@@ -63,16 +87,16 @@ def apply_priority(ast):
 
 def to_postfix(ast):
     result = []
+    operator = ""
     for element in ast:
         if isinstance(element, list):
             result.append(to_postfix(element))
         elif isinstance(element, Term):
             result.append(element.value)
         elif isinstance(element, Operator):
-            # Move operator to the end for postfix notation
-            result.insert(-1, element.value)
-    return ' '.join(result)
+            operator = element.value
+    return "".join(result)+operator
 
-parsed = apply_priority(parsed)
+parsed = apply_priority2(apply_priority1(parsed))
 postfix_expression = to_postfix(parsed)
 print(postfix_expression)
