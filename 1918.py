d169a5dce63acc26795d812f972918d97d223d6d
Myoun <aiden080605@gmail.com>
2025-01-23 15:37:36 +0900

    feat: Add new script for processing data in 1918.py

---
+++ 1918.py
@@ -0,0 +1,41 @@
+from dataclasses import dataclass
+from typing import Self
+
+expression = input()
+
+@dataclass
+class Term:
+    value: str
+
+@dataclass
+class Operator:
+    value: str
+
+parsed = []
+bracket = 0
+
+for i, c in enumerate(expression):
+    if c == '(':
+        bracket += 1
+        parsed.append([])
+    elif c == ')':
+        if bracket > 1:
+            last = parsed[-1]
+            del parsed[-1]
+            parsed[-1].append(last)
+        bracket -= 1
+
+    elif c == '*' or c == '/' or c == '+' or c == '-':
+        if bracket:
+            parsed[-1].append(Operator(c))
+        else:
+            parsed.append(Operator(c))
+    else:
+        # 문자
+        if bracket:
+            parsed[-1].append(Term(c))
+        else:
+            parsed.append(Term(c))
+
+
+print(parsed)
