55d1621c99caf708a8af4e382ca72a90c5581f9e
Myoun <aiden080605@gmail.com>
2025-01-23 16:18:23 +0900

    feat: Add operator priority handling in expression parsing

--- 1918.py
+++ 1918.py
@@ -14,6 +14,7 @@ class Operator:
 parsed = []
 bracket = 0
 
+# Parse
 for i, c in enumerate(expression):
     if c == '(':
         bracket += 1
@@ -24,22 +25,40 @@ for i, c in enumerate(expression):
             del parsed[-1]
             parsed[-1].append(last)
         bracket -= 1
-
-    elif c == '*' or c == '/':
-        if bracket == 0 or not isinstance(parsed[-1], list):
-            parsed.append([])
-        parsed[-1].append(Operator(c))
-    elif c == '+' or c == '-':
+    elif c == '*' or c == '/' or c == '+' or c == '-':
         if bracket:
             parsed[-1].append(Operator(c))
         else:
             parsed.append(Operator(c))
     else:
-        # 문자
         if bracket:
             parsed[-1].append(Term(c))
         else:
             parsed.append(Term(c))
 
+def apply_priority(ast):
+    new_ast=[]
+    p = 0
 
-print(parsed)
+    for i, e in enumerate(ast):
+
+        if p > 0:
+            p -= 1
+            continue
+
+        if isinstance(e, Operator) and (e.value == "*" or e.value =="/"):
+            prev = new_ast[-1]
+            del new_ast[-1]
+            next = ast[i+1]
+            if isinstance(ast[i+1], list):
+                next = apply_priority(ast[i+1])
+            new_ast.append([prev, e, next])
+            p = 1
+        elif isinstance(e, list):
+            new_ast.append(apply_priority(e))
+        else:
+            new_ast.append(e)
+    
+    return new_ast
+
+parsed = apply_priority(parsed)
